615435341e89bb90d3d7fa02c6b23abb8c4f7ea3
shakil <ahmadshakil22255@gmail.com>
2025-02-14 12:29:31 +0600

    update

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,57 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
+  const createNewUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const [user, setUser] = useState(null);
+  // console.log(user);
+
+  const userLogin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  const updateUserProfile = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  const authInfo = {
+    user,
+    setUser,
+    createNewUser,
+    userLogin,
+    logOut,
+    updateUserProfile,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
